9d149e6b7528ccb07fafc879f90416d5fdecafa7
cperryoh <46071730+cperryoh@users.noreply.github.com>
2025-04-03 16:19:49 -0400

    New alien works

---
+++ src/main/RedAlien.java
@@ -0,0 +1,48 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RedAlien extends  Alien{
+    public RedAlien(int x, int y, String direction) {
+        super(x, y, direction);
+    }
+
+    @Override
+    public void drawOn(Graphics2D g) {
+        this.image = Toolkit.getDefaultToolkit().getImage("RedAlien.png");
+        try {
+            this.image = ImageIO.read(new File("RedAlien.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.drawImage(image, x, y, width, height, null);
+    }
+
+    @Override
+    public void move(ArrayList<Platform> plats) {
+        this.contactWith(plats);
+        Random random = new Random();
+        boolean changeDir = random.nextInt(21)==20;
+        if(changeDir)
+            direction=direction.equals("-")?"+":"-";
+        if (direction == "-") {
+            if (directNum == 1) {
+                this.y += 10;
+            } else {
+                this.y -= 10;
+            }
+            this.x -= 15;
+        } else {
+
+            if (directNum == 2) {
+                this.y -= 10;
+            } else {
+                this.y += 10;
+            }
+            this.x += 15;
+        }
+    }
+}
