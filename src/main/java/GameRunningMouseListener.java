4b35756950cae80aac87ca7a73a41d834c7e314f
Carl Blome <blomecj@rose-hulman.edu>
2025-04-25 15:15:59 -0400

    Refused Bequest Refactoring Using the Adapters instead of Listeners

--- src/main/java/GameRunningMouseListener.java
+++ src/main/java/GameRunningMouseListener.java
@@ -1,7 +1,7 @@
-import javax.swing.event.MouseInputListener;
+import javax.swing.event.MouseInputAdapter;
 import java.awt.event.MouseEvent;
 
-public class GameRunningMouseListener implements MouseInputListener {
+public class GameRunningMouseListener extends MouseInputAdapter {
     Player player;
 
     public GameRunningMouseListener(Player player) {
@@ -9,35 +9,10 @@ public class GameRunningMouseListener implements MouseInputListener {
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
     public void mousePressed(MouseEvent e) {
         if (e.getButton() == MouseEvent.BUTTON1) {
             player.shoot();
         }
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-
     }
 
     @Override
