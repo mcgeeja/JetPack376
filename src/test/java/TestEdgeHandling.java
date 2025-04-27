bd2b47b60f10fbb3a765e1caa4be0bda0205e6fa
Cole <cperryoh@gmail.com>
2025-04-26 22:33:38 -0400

    Test player specifically

--- src/test/java/TestEdgeHandling.java
+++ src/test/java/TestEdgeHandling.java
@@ -7,33 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestEdgeHandling {
     @Test
     public void HandleLeftEdge (){
-        GameObject obj = new GameObject(1,1,10,10) {
-            @Override
-            public void drawOn(Graphics2D g) {
-
-            }
-
-            @Override
-            public void leftEdgeHit() {
-                x = 1920-10;
-            }
+        GameObject obj = new Player(1,1,1) {
         };
         obj.leftEdgeHit();
-        assertEquals(1920-10, obj.x);
+        assertEquals(1900, obj.x);
     }
     public void HandleRightEdge() {
-        GameObject obj = new GameObject(1919, 1, 10, 10) {
-            @Override
-            public void drawOn(Graphics2D g) {
-
-            }
-
-            @Override
-            public void rightEdgeHit() {
-                x = 0;
-            }
-        };
+        GameObject obj = new Player(1,1,1) ;
         obj.rightEdgeHit();
-        assertEquals(0, obj.x);
+        assertEquals(10, obj.x);
     }
     }
