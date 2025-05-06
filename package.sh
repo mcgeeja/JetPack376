b3117953cce8dd0535f371b7bc0967bbb91299ed
Cole <cperryoh@gmail.com>
2025-05-06 18:23:30 -0400

    Add shell package script

---
+++ package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir JetpackGame
+cp ./alien_type_1_2.0.png ./JetpackGame
+cp ./Alien_type_2_2.0.png ./JetpackGame
+cp ./Astronaut.png ./JetpackGame
+cp -r ./levels ./JetpackGame
+cp ./build/libs/JetPack375-1.0-SNAPSHOT.jar ./JetpackGame
