8120334a907df1a664a9c3ddcc2d13be2f7ff31d
mcgeeja <mcgeeja@rose-hulman.edu>
2025-05-12 15:58:04 -0400

    small final changes

--- src/main/java/BuildingPiece.java
+++ src/main/java/BuildingPiece.java
@@ -2,16 +2,14 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class BuildingPiece extends GameObject{
-
-	protected static final int size = 50;
 	
 	public BuildingPiece(int x, int y) {
-		super(x, y, size, size);
+		super(x, y, 40, 60);
 	}
 
 	public void drawOn(Graphics2D g) {
 		g.setColor(Color.MAGENTA);
-		g.fillRect(x, y, width, height);
+		g.fillRect(x, y-10, width, height);
 		g.setColor(Color.WHITE);
 	}
 
